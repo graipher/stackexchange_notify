cd8ad00d8672c5d083a691841984ac0a037571b7
Andreas Weiden <andreas.weiden@cern.ch>
2017-01-23 11:52:59 +0100

    Add downloading ico Add option max-questions to limit number of notifications placed in queue

--- se_notify.py
+++ se_notify.py
@@ -3,15 +3,18 @@
 # @Author: Andreas Weiden <andreas.weiden@gmail.com>
 # @Date:   2016-09-27 15:32:31
 # @Last modified by:   andreas
-# @Last modified time: 2016-10-10 11:59:15
+# @Last modified time: 2017-01-23 11:50:05
 
 
-import stackexchange
-import time
-import argcomplete
 import argparse
-from notifiers import *
+import os
+import time
 
+import requests
+
+import argcomplete
+import stackexchange
+from notifiers import *
 
 sites = [s for s in dir(stackexchange.sites) if not s.startswith('__')]
 
@@ -30,6 +33,8 @@ parser.add_argument('--start-time', type=int, default=1000,
                     help="How far back to start displaying in seconds (default: 1000)")
 parser.add_argument('--verbose', '-v', action='store_true',
                     help="Turn on printing of new questions")
+parser.add_argument('--max-questions', type=int, default=20,
+                    help="Maximum number of questions to display at a time.")
 argcomplete.autocomplete(parser)
 
 
@@ -62,10 +67,25 @@ def get_questions(site, query_delay, tag, start_time, max_questions=20):
 
 
 def get_icon(site):
-    import os
-    path = "{}/icons/{}.png".format(os.getcwd(), site.lower())
-    if os.path.isfile(path):
-        return path
+    site = site.lower()
+    path = os.path.join(os.getcwd(), "icons")
+
+    # Try to find an existing file
+    for extension in ("png", "ico"):
+        file_name = os.path.join(path, "{}.{}".format(site, extension))
+        if os.path.isfile(file_name):
+            return file_name
+
+    # Try downloading .ico from se
+    url = "https://cdn.sstatic.net/Sites/{}/img/favicon.ico"
+    r = requests.get(url.format(site), stream=True)
+    if r.status_code == 200:
+        with open(file_name, 'wb') as f:
+            for chunk in r:
+                f.write(chunk)
+        return file_name
+
+    # Fallback
     return "dialog-information"
 
 
@@ -75,7 +95,7 @@ def main():
     notifier = notifiers[args.notifier]
     if not args.icon:
         args.icon = get_icon(args.site)
-    for question in get_questions(args.site, args.delay, args.tag, args.start_time):
+    for question in get_questions(args.site, args.delay, args.tag, args.start_time, args.max_questions):
         notifier(question, args.icon)
         if args.verbose:
             print_notify(question)
